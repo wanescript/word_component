aa4fbf00bb72c2f2d32a2c7640e2db2f1f1921c4
Wane Smith <wanesmith7@gmail.com>
2019-07-19 01:39:17 -0400

    file changes

--- build/javascript/script1.js
+++ build/javascript/script1.js
@@ -1,10 +1,13 @@
 
-const vocabulary = ['programmer','development','software','hardware','memory','RAM','CPU','technology','loop','console','javascript','HTML','CSS','application','web development','IT','array','conditionals','objects','DNS','IP','server','design','routes','database','URL','URI','host','domain','hash','encryption','functions','cyber security','put','patch','get','http','delete','query','passwords','DHCP','root server','BIOS','ISP','users','OS','HTTPS','variables','code block','binary','buffer','load balancer','network','ping','trace router','pen testing','call stack','language','system design','user','vocabulary','pipeline','schema','modules','compiler','hash table','call stack','ROM','GIT','version control','certificate','project','string','DOM','API','MVC','technology','status code','parameters','call backs','bugs','debug','request','front-end','backend','range','RegEx','expressions','TTD','deployment','project','time management','file','upload','download','laptop','desktop','script','scripting','parser','float','integer','directory','variables','identifier','event listener','event handler','event','framework','nodes','DOM','AJAX','fetch','TCP'];
+const vocabulary = ['programmer','development','software','hardware','memory','RAM','CPU','technology','loop','console','javascript','HTML','CSS','application','web development','IT','array','conditionals','objects','DNS','IP','server','design','routes','database','URL','URI','host','domain','hash','encryption','functions','cyber security','put','patch','get','http','delete','query','passwords','DHCP','root server','BIOS','ISP','users','OS','HTTPS','variables','code block','binary','buffer','load balancer','network','ping','trace router','pen testing','call stack','language','system design','user','vocabulary','pipeline','schema','modules','compiler','hash table','call stack','ROM','GIT','version control','certificate','project','string','DOM','API','MVC','technology','status code','parameters','call backs','bugs','debug','request','front-end','backend','range','RegEx','expressions','TTD','deployment','project','time management','file','upload','download','laptop','desktop','script','scripting','parser','float','integer','directory','variables','identifier','event listener','event handler','event','framework','nodes','DOM','AJAX','fetch','TCP','socket','protocol','parameters','parser','children','child','parent','define','instanceof','inject','instantiate'];
 
 
 const timer = document.getElementById('timer');
 const wordCount = document.getElementById('wordCount');
 let counter = 0;
+
+wordCount.innerText= `Total vocabulary count: ${vocabulary.length}`;
+timer.innerText= `Now starting...`;
 
 function timerNotice() {
 
@@ -17,18 +20,18 @@ function timerNotice() {
       timer.innerText= `${counter}sec ago`;
     }
     
-    
-  
 }, 1000)};
 
 
 function startFunction(){
   
 setInterval(message,10000);
-setInterval(resetTimer,10000)
+setInterval(resetTimer,10000);
+
 }
 
 function resetTimer (){
+
 counter = 0;
  
 }
@@ -37,27 +40,24 @@ function message (){
 
 timer.innerText= `Current word.`
 
-
 }
   
 function words() {
- 
-setInterval(randomWord,10000)
+
+randomWord();
+setInterval(randomWord,10000);
 
 }
 
 function randomWord (){
   
-let randomVocabulary = vocabulary[Math.floor(Math.random()*vocabulary.length)];
-document.getElementById('output').innerText = randomVocabulary;
+let randomVocabulary = vocabulary[Math.floor(Math.random()*vocabulary.length)]
 
-console.log(randomVocabulary);
+document.getElementById('output').innerText = `${randomVocabulary}`;
+
+console.log(randomVocabulary);//testing purposes
+
 }
-
-
-output.innerText = `${vocabulary.length} vocabulary words`;
-wordCount.innerText= `Total vocabulary count:  ${vocabulary.length}`;
-timer.innerText= `Now starting...`;
 
 
 timerNotice();
